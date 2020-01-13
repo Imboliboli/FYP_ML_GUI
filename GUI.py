fac4f1338ceff1c56cefd84b8e09132e79e05be0
yonghao <fuyonghao94@gmail.com>
2020-01-13 14:24:45 +0800

    c1

--- GUI.py
+++ GUI.py
@@ -3,12 +3,18 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 
 LARGE_FONT = ("Verdana", 15)
+path = "/Users/geraldine/PycharmProjects/FYP_ML_GUI/images/images.jpeg"
+
+# start page image
+
+with Image.open(path) as img:
+    w, h = img.size
 
 
 class MyGui(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        container = tk.Frame(self)
+        container = tk.Frame(self,width =w+100, height = h+100)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -17,8 +23,12 @@ class MyGui(tk.Tk):
 
         for F in (StartPage, PageOne, PageTwo):
             frame = F(container, self)
+
             self.frames[F] = frame
-            frame.grid(row=1, column=0, sticky="w")
+            frame.grid(row=0, column=0)
+            frame.pack_propagate(0)
+
+
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
@@ -26,11 +36,12 @@ class MyGui(tk.Tk):
         frame.tkraise()
 
 
+
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        img = ImageTk.PhotoImage(Image.open("/Users/geraldine/PycharmProjects/FYP_ML_GUI/images/images.jpeg"))
-        label = tk.Label(self, image = img)
+        tk.Frame.__init__(self, parent, width=w+100, height=h+100,bg="RED")
+        img = ImageTk.PhotoImage(Image.open(path))
+        label = tk.Label(self, image=img)
         label.image = img
         label.pack()
 
@@ -44,7 +55,8 @@ class StartPage(tk.Frame):
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, width=w+100, height=h+100)
+
         label = tk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
@@ -58,7 +70,7 @@ class PageOne(tk.Frame):
 
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent,width=w+100, height=h+100)
         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
